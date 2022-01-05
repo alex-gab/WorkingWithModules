b7cce93b549e9453b6eb18a7e2068d7994b458f9
Alex Dragoi <dragoi.alex@gmail.com>
2022-01-05 10:41:12 +0200

    add exports

---
+++ public/js/sessionRepository.js
@@ -0,0 +1,17 @@
+const sessionURI = 'sessions.json';
+let sessionList = [];
+
+function getSessions() {
+    return new Promise(function (resolve, reject) {
+        var oReq = new XMLHttpRequest();
+        oReq.onload = function (e) {
+            sessionList = e.target.response;
+            resolve(sessionList);
+        };
+        oReq.open('GET', sessionURI, true);
+        oReq.responseType = 'json';
+        oReq.send();
+    })
+}
+
+export {getSessions}
